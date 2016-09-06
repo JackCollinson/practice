35ddfec44ffde50e122e4159b7442c07121fad34
Jack Collinson <jicollinson1@sheffield.ac.uk>
2016-09-06 09:43:46 +0100

    compiled warmup exercsises into single file

--- codingbat/Warmup-1/diff21.py
+++
@@ -1,7 +0,0 @@
-'''
-Given an int n, return the absolute difference between n and 21, except
-return double the absolute difference if n is over 21.
-'''
-
-def diff21(n):
-    return 2 * abs(21 - n) if n > 21 else abs(21 - n)
