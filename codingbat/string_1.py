2d40f52398355b71bf3bb3828a08d4079a3525d9
Jack Collinson <jicollinson1@sheffield.ac.uk>
2016-09-06 11:06:43 +0100

    string_1 solutions

--- codingbat/string_1.py
+++ codingbat/string_1.py
@@ -24,8 +24,79 @@ def make_tags(tag, word):
     return '<' + tag + '>' + word + '</' + tag + '>'
 
 '''
-Given an "out" string length 4, such as "<<>>", and a word, return a new string 
+Given an "out" string length 4, such as "<<>>", and a word, return a new string
 where the word is in the middle of the out string, e.g. "<<word>>".
 '''
+
 def make_out_word(out, word):
     return out[:2] + word + out[2:]
+
+'''
+Given a string, return a new string made of 3 copies of the last 2 chars of the
+original string. The string length will be at least 2.
+'''
+
+def extra_end(str):
+    end = str[-2:]
+    return end + end + end
+
+'''
+Given a string, return the string made of its first two chars, so the String
+"Hello" yields "He". If the string is shorter than length 2, return whatever
+there is, so "X" yields "X", and the empty string "" yields the empty string "".
+'''
+
+def first_two(str):
+    return str if len(str) < 2 else str[:2]
+
+'''
+Given a string of even length, return the first half. So the string "WooHoo"
+yields "Woo".
+'''
+
+def first_half(str):
+    return str[:len(str)/2]
+
+'''
+Given a string, return a version without the first and last char, so "Hello"
+yields "ell". The string length will be at least 2.
+'''
+
+def without_end(str):
+    return str[1:-1]
+
+'''
+Given 2 strings, a and b, return a string of the form short+long+short, with the
+shorter string on the outside and the longer string on the inside. The strings
+will not be the same length, but they may be empty (length 0).
+'''
+
+def combo_string(a, b):
+    if len(a) > len(b):
+        longer = a
+        shorter = b
+    else:
+        longer = b
+        shorter = a
+    return shorter + longer + shorter
+
+'''
+Given 2 strings, return their concatenation, except omit the first char of each.
+The strings will be at least length 1.
+'''
+
+def non_start(a, b):
+    return a[1:] + b[1:]
+
+'''
+Given a string, return a "rotated left 2" version where the first 2 chars are
+moved to the end. The string length will be at least 2.
+'''
+
+def left2(str):
+    return rotate(2, str)
+
+def rotate(n, str):
+    for i in range(n):
+        str = str[1:] + str[0]
+    return shorter
